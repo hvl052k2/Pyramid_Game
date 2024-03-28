ac0c7a0f5d444e0682dc420871ec8a9cd7413e85
H.V.Linh <halinh052002@gmail.com>
2024-03-28 12:54:59 +0700

    add native splash screen

--- lib/src/constants/image_strings.dart
+++ lib/src/constants/image_strings.dart
@@ -3,3 +3,4 @@ const String signUpLogo = "assets/images/logo_sign_up.png";
 const String avatarImage = "assets/images/default_avatar.png";
 const String wallImage = "assets/images/chessboard2.png";
 const String forgotPasswordImage = "assets/images/forgot_password_image.png";
+const String splashImage = "assets/images/splash_image.png";
